37fc556a244794be7540a60f44c792b227ec2c49
Petr Bažout <bazout.p@gmail.com>
2016-12-20 22:17:00 +0100

    Add build process

---
+++ build/index.js
@@ -0,0 +1,4 @@
+"use strict";
+var some_file_1 = require("./some-file");
+console.log(some_file_1.foo);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import {foo} from './some-file';
+
+console.log(foo)
